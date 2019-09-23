11530f8faacb0ae6ae714df37bb885d7fe0d5ba0
ziyingwong <zy_508@hotmail.com>
2019-09-23 23:46:33 +0800

    added progress bar

--- app/src/main/java/com/example/mobile/General_PlayScenes.kt
+++ app/src/main/java/com/example/mobile/General_PlayScenes.kt
@@ -4,30 +4,37 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 
 class General_PlayScenes : AppCompatActivity() {
+
     var ipAdd = "10.0.2.2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.general_webview)
         var webView = findViewById<WebView>(R.id.webView)
+        var progressBar = findViewById<ProgressBar>(R.id.progressBarWebView)
         webView.settings.javaScriptEnabled = true
         var list = intent.getStringArrayListExtra("list")
-        var selected = intent.getIntExtra("selected",0)
+        var selected = intent.getIntExtra("selected", 0)
         var url = "http://${ipAdd}:3000/board-viewer/"
-
+        webView.visibility = View.INVISIBLE
+        progressBar.visibility = View.VISIBLE
         webView.setWebViewClient(object : WebViewClient() {
             override fun onPageFinished(view: WebView, url: String) {
 
-                webView.loadUrl(
+                view.loadUrl(
                     "javascript:document.querySelector(\"things-app\").shadowRoot.querySelector(\"header-bar\").remove();" +
                             "document.querySelector(\"things-app\").shadowRoot.querySelector(\"board-viewer-page\").shadowRoot.querySelector(\"board-viewer\").shadowRoot.querySelector(\"mwc-fab\").remove();" +
                             "document.querySelector(\"things-app\").shadowRoot.querySelector(\"footer-bar\").remove();"
                 )
+                progressBar.visibility = View.INVISIBLE
+                webView.visibility = View.VISIBLE
 
 
             }
@@ -38,13 +45,13 @@ class General_PlayScenes : AppCompatActivity() {
         webView.loadUrl(url + list.get(selected))
 
         var handler = Handler()
-        var delay: Long = 20000
+        var delay: Long = 30000
         handler.postDelayed(
             object : Runnable {
                 override fun run() {
                     val intent = Intent(this@General_PlayScenes, General_PlayScenes::class.java)
                     intent.putExtra("list", list)
-                    intent.putExtra("selected", selected+1)
+                    intent.putExtra("selected", selected + 1)
                     intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
                     startActivity(intent)
                 }
@@ -52,13 +59,4 @@ class General_PlayScenes : AppCompatActivity() {
         )
 
     }
-
-    override fun onBackPressed() {
-        val intent = Intent(this@General_PlayScenes, Admin_Main::class.java)
-        intent.putExtra("selectedFragment",R.id.nav_admin_playgroup)
-        startActivity(intent)
-        finish()
-
-    }
-
 }
--- app/src/main/java/com/example/mobile/General_ViewScene.kt
+++ app/src/main/java/com/example/mobile/General_ViewScene.kt
@@ -1,9 +1,11 @@
 package com.example.mobile
 
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 
 class General_ViewScene : AppCompatActivity() {
 
@@ -13,9 +15,12 @@ class General_ViewScene : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.general_webview)
         var webView = findViewById<WebView>(R.id.webView)
+        var progessBar = findViewById<ProgressBar>(R.id.progressBarWebView)
         webView.settings.javaScriptEnabled = true
         var id = intent.getStringExtra("id")
         var url = "http://${ipAdd}:3000/board-viewer/${id}"
+        webView.visibility = View.INVISIBLE
+        progessBar.visibility = View.VISIBLE
         webView.setWebViewClient(object : WebViewClient() {
 
 
@@ -23,8 +28,12 @@ class General_ViewScene : AppCompatActivity() {
                 webView.loadUrl("javascript:document.querySelector(\"things-app\").shadowRoot.querySelector(\"header-bar\").remove();" +
                         "document.querySelector(\"things-app\").shadowRoot.querySelector(\"board-viewer-page\").shadowRoot.querySelector(\"board-viewer\").shadowRoot.querySelector(\"mwc-fab\").remove();" +
                         "document.querySelector(\"things-app\").shadowRoot.querySelector(\"footer-bar\").remove();")
+                progessBar.visibility = View.INVISIBLE
+                webView.visibility = View.VISIBLE
+
 
             }
+
         })
         webView.loadUrl(url)
     }
