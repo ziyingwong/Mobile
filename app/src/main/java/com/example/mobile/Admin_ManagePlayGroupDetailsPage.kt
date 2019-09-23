4264c04b27ebd4095fee6b2e4487f97b4238e02a
ziyingwong <zy_508@hotmail.com>
2019-09-23 23:45:45 +0800

    added manage play group adapter

--- app/src/main/java/com/example/mobile/Admin_ManagePlayGroupDetailsPage.kt
+++ app/src/main/java/com/example/mobile/Admin_ManagePlayGroupDetailsPage.kt
@@ -1,5 +1,6 @@
 package com.example.mobile
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -12,20 +13,21 @@ import com.google.firebase.firestore.FirebaseFirestore
 class Admin_ManagePlayGroupDetailsPage:AppCompatActivity() {
 
     var db = FirebaseFirestore.getInstance()
-    lateinit var adapter : Admin_ManageGroupDetails_Adapter
+    lateinit var adapter : Admin_ManagePlayGroupDetails_Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.admin_list_addbutton_fragment)
         var name = intent.getStringExtra("name")
         var id = intent.getStringExtra("id")
+        var list = intent.getStringArrayListExtra("list")
         findViewById<TextView>(R.id.fragmentTitle).text = name
 
         var query = db.collection("scene").whereArrayContains("playgroup",id)
         var options = FirestoreRecyclerOptions.Builder<Object_Scene>()
             .setQuery(query,Object_Scene::class.java)
             .build()
-        adapter = Admin_ManageGroupDetails_Adapter(options)
+        adapter = Admin_ManagePlayGroupDetails_Adapter(options,id)
         var recycler = findViewById<RecyclerView>(R.id.fragmentRecycler)
         recycler.layoutManager = LinearLayoutManager(this)
         recycler.adapter = adapter
@@ -46,8 +48,11 @@ class Admin_ManagePlayGroupDetailsPage:AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
+        val intent = Intent(this, Admin_Main::class.java)
+        intent.putExtra("selectedFragment",R.id.nav_admin_playgroup)
+        startActivity(intent)
         finish()
+
     }
 
 
---
+++ app/src/main/java/com/example/mobile/Admin_ManagePlayGroupDetails_Adapter.kt
@@ -0,0 +1,54 @@
+package com.example.mobile
+
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+
+class Admin_ManagePlayGroupDetails_Adapter(
+    options: FirestoreRecyclerOptions<Object_Scene>,
+    id: String
+) :
+    FirestoreRecyclerAdapter<Object_Scene, Admin_ManagePlayGroupDetails_Adapter.ViewHolder>(options) {
+    var id = id
+    var db = FirebaseFirestore.getInstance()
+
+    override fun onCreateViewHolder(
+        p0: ViewGroup,
+        p1: Int
+    ): Admin_ManagePlayGroupDetails_Adapter.ViewHolder {
+        val view =
+            LayoutInflater.from(p0.context).inflate(R.layout.admin_list_fragment_card, p0, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, model: Object_Scene) {
+        holder.id.text = model.name
+        holder.itemView.setOnClickListener { view ->
+
+            db.collection("PlayGroup").document(id).get().addOnSuccessListener { doc ->
+                if (doc.exists()) {
+                    val intent = Intent(view.context, General_PlayScenes::class.java)
+                    var scenes = doc.get("scene") as ArrayList<String>
+                    intent.putExtra("selected", position)
+                    intent.putExtra("list", scenes)
+                    Log.e("myTag","${id} : ${scenes}")
+                    intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
+                    view.context.startActivity(intent)
+
+                }
+            }
+
+        }
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var id = itemView.findViewById<TextView>(R.id.dataA)
+    }
+}
